9799ce147d754637b22881e74dc75908a4a064a2
kavinesh <kavineshkavinesh860@gmail.com>
2024-08-13 22:31:47 +0530

    main controller js file

--- Forkify_Rebuilt/src/js/controller.js
+++ Forkify_Rebuilt/src/js/controller.js
@@ -3,6 +3,7 @@ import recipeView from "./views/recipeView";
 import resultsView from "./views/resultsView.js";
 import searchView from "./views/searchView.js";
 import paginationView from "./views/paginationView.js";
+import bookmarkView from "./views/bookmarkView.js";
 
 const controlRecipes = async function () {
   try {
@@ -11,13 +12,15 @@ const controlRecipes = async function () {
     if (!id) return;
     recipeView.renderSpinner();
 
+    resultsView.update(model.getPageLoadResults());
+    bookmarkView.update(model.state.bookmarks);
+
     // 1) Load recipe and stored in recipe object
     await model.loadRecipe(id);
 
     //2) Render recipe in the UI
     recipeView.render(model.state.recipe);
   } catch (err) {
-    console.error(err);
     recipeView.renderError();
   }
 };
@@ -36,10 +39,38 @@ const controlSearchResults = async function () {
   }
 };
 
+const controlPageNavigation = function (page) {
+  try {
+    resultsView.render(model.getPageLoadResults(page));
+
+    paginationView.render(model.state.search);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const controlServings = async function (servings) {
+  model.updateServings(servings);
+
+  // recipeView.render(model.state.recipe);
+  recipeView.update(model.state.recipe);
+};
+
+const controlAddBookmarks = function () {
+  if (!model.state.recipe.bookmarked) model.addBookmarks(model.state.recipe);
+  else model.removeBookmarks(model.state.recipe.id);
+
+  recipeView.update(model.state.recipe);
+
+  bookmarkView.render(model.state.bookmarks);
+};
+
 const init = async function () {
   recipeView.addHanlerRender(controlRecipes);
+  recipeView.addHanlerServings(controlServings);
+  recipeView.addHanlerAddBookmark(controlAddBookmarks);
   searchView.addHanlerRender(controlSearchResults);
-  paginationView.addHandlerPagination();
+  paginationView.addHandlerPagination(controlPageNavigation);
 };
 
 init();
